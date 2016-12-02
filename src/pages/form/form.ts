18a9910323bc22075c3a3fe9bb73ef84a0bf0803
mboniface <mboniface@financeactive.com>
2016-12-02 16:17:40 +0100

    add form page

---
+++ src/pages/form/form.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-form',
+    templateUrl: 'form.html'
+})
+export class FormPage {
+
+    model = {};
+
+    constructor() {
+    }
+
+
+
+
+}
